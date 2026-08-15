9b1a5eed89be20d630f2fe08ae7c3c9b0ca7e96d
Hamro-kirana-pasal <gckamal678@gmail.com>
2026-08-15 21:46:47 +0530

    Refactor service worker for cache management

--- sw.js
+++ sw.js
@@ -1,60 +1,26 @@
-const CACHE_NAME = 'patshala-cache-v3';
-const urlsToCache = [
-  './',
-  './index.html',
-  './pahada.html',
-  './alphabet.html',
-  './image.html',
-  './vowels.html',
-  './imegevowels.html',
-  './consonants.html',
-  './imageconsonants.html',
-  './barahakhari.html',
-  './colors.html',
-  './fruits-vegetables.html',
-  './animals-birds.html',
-  './body-parts.html',
-  './vehicles.html',
-  './days-months.html',
-  './numbers.html',
-  './nepal.png',
-  './192.png',
-  './512.png',
-  './manifest.json'
-];
+const CACHE_NAME = 'patshala-v5';
 
-// Service Worker Install
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
-  );
+// Install Event
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-// Activate & Clear Old Cache
-self.addEventListener('activate', event => {
+// Activate Event (पुराना सबै अड्किएका Cache हटाइदिने)
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((keys) => {
       return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
+        keys.map((key) => caches.delete(key))
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
-// Fetch Offline Support
-self.addEventListener('fetch', event => {
+// Fetch Event (पहिला Internet/Server बाट खोल्ने, नभए मात्र Cache)
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
