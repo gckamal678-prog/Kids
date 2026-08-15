ab236029ef7135af272aeb469080c4bdf9013197
Hamro-kirana-pasal <gckamal678@gmail.com>
2026-08-15 21:37:32 +0530

    Update service worker cache version and assets
    Updated cache version and added new URLs to cache.

--- sw.js
+++ sw.js
@@ -1,10 +1,26 @@
-const CACHE_NAME = 'patshala-cache-v1';
+const CACHE_NAME = 'patshala-cache-v3';
 const urlsToCache = [
   './',
   './index.html',
+  './pahada.html',
+  './alphabet.html',
+  './image.html',
+  './vowels.html',
+  './imegevowels.html',
+  './consonants.html',
+  './imageconsonants.html',
+  './barahakhari.html',
+  './colors.html',
+  './fruits-vegetables.html',
+  './animals-birds.html',
+  './body-parts.html',
+  './vehicles.html',
+  './days-months.html',
+  './numbers.html',
   './nepal.png',
   './192.png',
-  ./512.png'
+  './512.png',
+  './manifest.json'
 ];
 
 // Service Worker Install
@@ -14,10 +30,26 @@ self.addEventListener('install', event => {
       .then(cache => {
         return cache.addAll(urlsToCache);
       })
+      .then(() => self.skipWaiting())
   );
 });
 
-// Fetching Assets (Offline Work)
+// Activate & Clear Old Cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Offline Support
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
